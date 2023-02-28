69df2ffe9b58a6bcc06b0c3f9c05aed021d9b2bf
Vi2345 <118724274+Vi2345@users.noreply.github.com>
2023-02-28 19:09:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function add(){
+    var num1= Number(document.getElementById("num1").value);
+    var num2= Number(document.getElementById("num2").value);
+    var add=num1+num2;
+    alert(add);
+
+}
